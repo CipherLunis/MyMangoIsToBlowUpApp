6a56330216f05433b9a2ab8e06055b4ad61a5b93
Cipher Lunis <cipheryxinquiries@gmail.com>
2024-01-06 20:36:56 -0600

    Initial Commit

---
+++ MyMangoIsToBlowUp/MyMangoIsToBlowUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMangoIsToBlowUpApp.swift
+//  MyMangoIsToBlowUp
+//
+//  Created by Cipher Lunis on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMangoIsToBlowUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
